529e09a4f796971baaa6d4359a6b80ed2198f512
analistaKim <161110171+analistaKim@users.noreply.github.com>
2024-02-24 16:15:04 +0900

    Strat Python Study
    파이썬 공부 시작하는 날
    데이터 분석가가 최종 목표지만 공부하는 동안 여러 분야에 대해서 배워보며
    이 분야에 대해 알아갈 예정

---
+++ pythontest_240224.py
@@ -0,0 +1,6 @@
+# 텍스트 파일로 된 소스코드를 파이썬 형식으로 확장자 변경하는 데 시간을 좀 썼음
+# 그냥 텍스트 파일을 편집기에서 열고 file-save as... 을 누른 뒤 형식 설정하면 된다.
+
+print("Hello Coding python")
+print("Hello Coding python")
+print("Hello Coding python")
